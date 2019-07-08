3ce3bbf96aac00fe15f03e0428f28e9b5224108a
Ian Cullinane <ian.cullinane@gmail.com>
2019-07-08 14:53:28 -0400

    remove provider

--- generic-s3.tf
+++ generic-s3.tf
@@ -1,8 +1,4 @@
-provider "aws" {
-    region = "${var.aws_region}"
-    access_key = "${var.aws_access_key}"
-    secret_key = "${var.aws_secret_key}"
-}
+
 
 resource "aws_s3_bucket" "b" {
   bucket = "${var.name}"
--- variables.tf
+++ variables.tf
@@ -1,19 +1,5 @@
 
 
-variable "aws_access_key" {
-    description = "The AWS access key."
-    default = "<...>"
-}
-
-variable "aws_secret_key" {
-    description = "The AWS secret key."
-    default = "<...>"
-}
-#
-variable "aws_region" {
-    description = "The AWS region to create resources in."
-    default = "us-east-2"
-}
 
 variable "vpc_id" {
     description = "VPC to connect to, used for a security group"
