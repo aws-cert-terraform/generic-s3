231f04be6d593faa18efcd1e11a99df2a06ebc1d
Ian Cullinane <ian.cullinane@gmail.com>
2019-07-08 17:29:24 -0400

    char typo

--- generic-s3.tf
+++ generic-s3.tf
@@ -4,7 +4,7 @@ resource "aws_s3_bucket" "b" {
   bucket = "${var.name}"
 
   
-  acl    = "${var.public ? 'public-read' : 'private'}"
+  acl    = "${var.public ? "public-read" : "private"}"
 
   tags = {
     Name        = "${var.prefix}${var.owner}"
