9a93ef0f43a57440aaca352f8065881e9c134fa2
Ian Cullinane <icullinane@turbine.com>
2019-07-25 16:58:43 -0400

    add more outputs

---
+++ output.tf
@@ -0,0 +1,14 @@
+
+output "bucket_id" {
+    value = "${aws_s3_bucket.b.id}"
+}
+
+
+output "bucket_arn" {
+    value = "${aws_s3_bucket.b.arn}"
+}
+
+
+output "bucket_domain_name" {
+    value = "${aws_s3_bucket.b.bucket_domain_name}"
+}
