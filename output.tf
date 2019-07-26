16724164876e5f0ffbbef314c5915e35ce55c215
Ian Cullinane <icullinane@turbine.com>
2019-07-26 10:02:56 -0400

    parsed bucket name

--- output.tf
+++ output.tf
@@ -10,5 +10,7 @@ output "bucket_arn" {
 
 
 output "bucket_domain_name" {
-    value = "${aws_s3_bucket.b.bucket_domain_name}"
+    value = element(split(".",aws_s3_bucket.b.bucket_domain_name), 0)
 }
+
+
